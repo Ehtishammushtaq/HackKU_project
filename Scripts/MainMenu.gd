573f7de590b4f5b54d56f38a3df654d6df31efbf
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2023-04-15 04:39:48 -0500

    Removing Bugs
    Some path errors after merging onto main, resolved conflicts

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_new_game_pressed():
-	get_tree().change_scene_to_file("res://Scenes/GamePlay.tscn")# Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/MainScene.tscn")# Replace with function body.
 
 
 func _on_exit_pressed():
