83d6e08c5df2674fe47f3ec4e38d66dfe80179fb
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2023-04-14 21:58:21 -0500

    Added placeholder assets
    Added placeholder assets and basic player mechanics

--- Scenes/Scripts/GamePlay.gd
+++ Scenes/Scripts/GamePlay.gd
@@ -1,26 +1,25 @@
 extends Node2D
 @onready var Rocket: TextureRect = $Rocket
 
-
-
+var is_held = false
+var change=10
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
+	Rocket.position = Vector2(100,850)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-
-func _unhandled_input(event):
 	var my_position = Rocket.position
-	var TW = create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN_OUT)
-	if event.is_action_pressed("ui_up"):
-		TW.tween_property(Rocket, "position", Vector2(my_position.x, my_position.y-100), 0.01)
-	elif event.is_action_pressed("ui_down"):
-		TW.tween_property(Rocket, "position", Vector2(my_position.x, my_position.y+100), 0.01)
-	elif event.is_action_pressed("ui_right"):
-		TW.tween_property(Rocket, "position", Vector2(my_position.x+100, my_position.y), 0.01)
-	elif event.is_action_pressed("ui_left"):
-		TW.tween_property(Rocket, "position", Vector2(my_position.x-100, my_position.y), 0.01)
+	var TW = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	if Input.is_action_pressed("ui_up"):
+		TW.tween_property(Rocket, "position", Vector2(my_position.x, my_position.y-change), 0.01)
+	elif Input.is_action_pressed("ui_down"):
+		TW.tween_property(Rocket, "position", Vector2(my_position.x, my_position.y+change), 0.01)
+	elif Input.is_action_pressed("ui_right"):
+		TW.parallel().tween_property(Rocket, "position", Vector2(my_position.x+change, my_position.y), 0.01)
+	elif Input.is_action_pressed("ui_left"):
+		TW.parallel().tween_property(Rocket, "position", Vector2(my_position.x-change, my_position.y), 0.01)
+	else:
+		TW.parallel().tween_property(Rocket, "position", Vector2(100, 850), 1)
+
+		
