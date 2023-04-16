87e55ea962760cfc4742bae9cdeeda12684bb827
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2023-04-15 21:30:00 -0500

    Added right wall
    Added right wall

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,9 +1,9 @@
 extends Control
-
+@onready var NewGame: TextureRect = $NewGameOv
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,3 +22,39 @@ func _on_exit_pressed():
 
 func _on_setting_pressed():
 	get_tree().change_scene_to_file("res://Scenes/SettingScreen.tscn")
+
+
+func _on_new_game_ov_mouse_entered():
+	print("new game enreredd")
+	var TW = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	TW.tween_property(NewGame, "scale", Vector2(0.65,0.65), 0.3)
+
+
+func _on_new_game_ov_mouse_exited():
+	print("new game exited")
+	var TW = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	TW.tween_property(NewGame, "scale",Vector2(0.6,0.61) , 0.3)
+
+
+func _on_settings_ov_mouse_entered():
+	pass
+
+
+func _on_settings_ov_mouse_exited():
+	pass # Replace with function body.
+
+
+func _on_scores_ov_mouse_entered():
+	pass # Replace with function body.
+
+
+func _on_scores_ov_mouse_exited():
+	pass # Replace with function body.
+
+
+func _on_exit_ov_mouse_entered():
+	pass # Replace with function body.
+
+
+func _on_exit_ov_mouse_exited():
+	pass # Replace with function body.
