5a75c195800af39a75727c9279a2b983840dd08a
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2023-04-15 19:53:19 -0500

    Boundary wall added for collision
    Boundary wall added for collision

---
+++ Scripts/OpeningScene.gd
@@ -0,0 +1,22 @@
+extends Node2D
+var ticks = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	ticks+=1
+	if ticks ==20:
+		$Space.hide()
+	elif ticks == 40:
+		$Space.show()
+		ticks=0
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://Scenes/MainScene.tscn")
+	elif event.is_action_pressed("ui_cancel"):
+		get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
