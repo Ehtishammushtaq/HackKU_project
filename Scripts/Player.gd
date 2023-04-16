fe1876fe97cc93d440ce129d9ac188183fdd1d03
Nikhil-Singla <nsingla3.14@gmail.com>
2023-04-15 22:27:55 -0500

    Added Movement Functionality. Sprite animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,14 @@
 extends Node2D
+
 @onready var Character: CharacterBody2D = $Character
 @onready var Circle: Sprite2D = $Circle
+@onready var animated_sprite = $Character/AnimatedSprite2D
+
 var is_held = false
 var ychange=30
 var xchange=0.5
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
 	Character.position = Vector2(100,600)
 
@@ -13,11 +17,14 @@ func _process(delta):
 	var my_position = Character.position
 	var TW = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
 	TW.tween_property(Character, "position", Vector2(my_position.x+xchange, my_position.y), 0.01)
-	
+	animated_sprite.play("IdleNoInput")
+
 	if Input.is_action_pressed("ui_up"):
+		animated_sprite.play("GoingUp")
 		if Global.TopWallTouched == false:
 			TW.tween_property(Character, "position", Vector2(my_position.x, my_position.y-ychange), 0.01)
 	elif Input.is_action_pressed("ui_down"):
+		animated_sprite.play("GoingDown")
 		if Global.BottomWallTouched== false:
 			TW.tween_property(Character, "position", Vector2(my_position.x, my_position.y+ychange), 0.01)
 	elif Input.is_action_pressed("ui_right"):
